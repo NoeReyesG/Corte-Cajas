a7dd7e2ea19b69966c9ee2fdd1428d3c2e906dfb
NoeReyesG <noe1513@gmail.com>
2023-12-26 17:13:39 -0600

    preparando para imprimir ficcha de deposito

--- src/app/bank-deposit/bank-deposit.component.ts
+++ src/app/bank-deposit/bank-deposit.component.ts
@@ -1,4 +1,4 @@
-import { FormatWidth } from '@angular/common';
+
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { from } from 'rxjs';
@@ -22,16 +22,12 @@ export class BankDepositComponent implements OnInit{
   bankDepositForm: FormGroup = this.fb.group({
     cashierName: this.fb.control('Noé Reyes'),
     tellerWindowNumber: this.fb.control(1),
-    checks1 : this.fb.array([]),
-    checks2 : this.fb.array([]),
-    checks3 : this.fb.array([])
+    checks : this.fb.array([]),
     
   })
 
-  get checks1(){ return this.bankDepositForm.controls['checks1'] as FormArray}
-  get checks2(){ return this.bankDepositForm.controls['checks2'] as FormArray}
-  get checks3(){ return this.bankDepositForm.controls['checks3'] as FormArray}
-
+  get checks(){ return this.bankDepositForm.controls['checks'] as FormArray}
+  
   
 
   ngOnInit(): void {
@@ -40,43 +36,30 @@ export class BankDepositComponent implements OnInit{
     if (sessionStorage.getItem('finalCashTotal')){
       this.finalCashTotal = Number(sessionStorage.getItem('finalCashTotal')); 
     }
-    for (let i: number = 0; i <= 9; i++){
-      this.checks1.push(this.fb.group({
+    for (let i: number = 0; i <= 29; i++){
+      this.checks.push(this.fb.group({
         number: [undefined],
         value: [undefined],
       }));
-      this.checks2.push(this.fb.group({
-        number: [undefined],
-        value: [undefined],
-      }));
-      this.checks3.push(this.fb.group({
-        number: [undefined],
-        value: [undefined],
-      }));
+      
     }
   }
 
   suma(): void{
     this.numberChecks = 0;
     this.sumChecksTotal = 0;
-    console.log(this.checks1.at(0).value);
 
-      for (let i: number = 0; i <= 9; i++){
+      for (let i: number = 0; i <= 29; i++){
       
-        if (this.checks1.at(i).value.number != null && this.checks1.at(i).value.value != null) {
+        if (this.checks.at(i).value.number != null && this.checks.at(i).value.value != null) {
           this.numberChecks= this.numberChecks+1;
-          this.sumChecksTotal = this.sumChecksTotal +  this.checks1.at(i).value.value;
-        }
-
-        if (this.checks2.at(i).value.number != null && this.checks2.at(i).value.value != null) {
-          this.numberChecks= this.numberChecks+1; 
-          this.sumChecksTotal = this.sumChecksTotal +  this.checks2.at(i).value.value;
-        }
-        if (this.checks3.at(i).value.number != null && this.checks3.at(i).value.value != null) {
-          this.numberChecks= this.numberChecks+1;
-          this.sumChecksTotal = this.sumChecksTotal +  this.checks3.at(i).value.value; 
+          this.sumChecksTotal = this.sumChecksTotal +  this.checks.at(i).value.value;
         }
       }    
+  }
+
+  print():void {
+    window.print();
   }
 
 }
