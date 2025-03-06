633f016900497565a617a4f2d7b1d8e48d2b9922
NoeReyesG <noe1513@gmail.com>
2025-03-06 17:14:57 -0600

    Avisos

--- src/app/avisos/avisos.component.ts
+++ src/app/avisos/avisos.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as confetti from 'canvas-confetti';
 
 @Component({
   selector: 'app-avisos',
   templateUrl: './avisos.component.html',
   styleUrls: ['./avisos.component.css']
 })
-export class AvisosComponent {
+export class AvisosComponent implements OnInit {
 
+ 
+  days=0;
+  hours=0; 
+  minutes=0;
+  seconds=0;
+  
+  runInterval(){
+    setInterval(()=>{
+      const contDownTime = new Date('March 28, 2025 23:59:00').getTime();
+      const now = new Date().getTime();
+      const distance = contDownTime-now;
+      this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    }, 100)
+  }
+  
+   
+
+  ngOnInit() {
+    this.runInterval();
+  }
 }
+
+
